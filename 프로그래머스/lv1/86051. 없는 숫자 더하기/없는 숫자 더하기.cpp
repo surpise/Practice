5ab9d2b774327848e9a6fb818c35048adf09406a
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-03-15 18:12:13 +0900

    Update 없는 숫자 더하기.cpp

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -1,11 +1,10 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
 int solution(vector<int> numbers) {
     int answer = 45;
-    sort(numbers.begin(), numbers.end());
     for(int i = 0; i < numbers.size(); i++){
         answer -= numbers[i];
     }
