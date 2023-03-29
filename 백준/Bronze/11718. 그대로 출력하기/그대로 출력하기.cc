9e558e70b500981c402fa257572feddfe0708abd
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-03-29 21:45:24 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    while(1){
+        getline(cin, str);
+        if(str == "") break;
+        cout << str << endl;
+    }
+}
