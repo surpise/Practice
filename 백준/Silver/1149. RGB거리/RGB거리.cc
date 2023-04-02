5c1b8916a4f610fd9b494b00dfa1745bcda8ccf5
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-04-02 22:22:46 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[1001][3];
+int main(){
+    int n, ans;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < 3; j++) cin >> dp[i][j];
+    }
+
+    for(int i = 1; i <= n; i++){
+        dp[i][0] += (dp[i - 1][1] < dp[i - 1][2]) ? dp[i - 1][1] : dp[i - 1][2];
+        dp[i][1] += (dp[i - 1][0] < dp[i - 1][2]) ? dp[i - 1][0] : dp[i - 1][2];
+        dp[i][2] += (dp[i - 1][0] < dp[i - 1][1]) ? dp[i - 1][0] : dp[i - 1][1];
+    }
+    ans = (dp[n][0] < dp[n][1]) ? dp[n][0] : dp[n][1];
+    ans = (ans < dp[n][2]) ? ans : dp[n][2];
+    cout << ans;
+}
