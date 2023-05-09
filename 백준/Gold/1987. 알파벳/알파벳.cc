16796b4edb0af786b0427dc903b085213e1ac102
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-05-09 13:39:27 +0900

    [Gold IV] Title: 알파벳, Time: 472 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int r, c, ans;
+char arr[20][20];
+bool vAlpha[26];
+
+void dfs(int _r, int _c, int cnt){
+    if(!vAlpha[arr[_r][_c] - 'A']){
+        vAlpha[arr[_r][_c] - 'A'] = 1;
+        if(_r - 1 >= 0) dfs(_r - 1, _c, cnt + 1);
+        if(_c - 1 >= 0) dfs(_r, _c - 1 , cnt + 1);
+        if(_r + 1 < r) dfs(_r + 1, _c, cnt + 1);
+        if(_c + 1 < c) dfs(_r, _c + 1, cnt + 1);
+        vAlpha[arr[_r][_c] - 'A'] = 0;
+    }
+    else{
+        if(cnt > ans) ans = cnt;
+    }
+}
+
+int main(){
+    cin >> r >> c;
+    for(int i = 0; i < r; i++){
+        for(int j = 0; j < c; j++) cin >> arr[i][j];
+    }
+    dfs(0, 0, 0);
+    cout << ans;
+}
