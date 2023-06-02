fb230a9802ff9f456265db5293fcb16118f41c98
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-06-02 09:36:50 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, x;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        cout << x << " " << x << '\n';
+    }
+}
