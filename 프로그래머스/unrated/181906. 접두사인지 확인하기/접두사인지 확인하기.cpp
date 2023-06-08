18903ea8324af25ec645ae22765fb868474fb3ed
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-06-08 11:37:42 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    int answer = 1;
+    for(int i = 0; i < is_prefix.size(); i++){
+        if(my_string[i] != is_prefix[i]) answer = 0;
+    }
+    return answer;
+}
