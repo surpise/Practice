ecd6af2809362cab7376dcb3311e78a38b037fb6
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-06-10 13:23:10 +0900

    [Gold III] Title: 선분 교차 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+struct point{
+    long long x;
+    long long y;
+};
+
+int ccw(point a, point b, point c){
+    long long t1 = a.x * b.y + b.x * c.y + c.x * a.y;
+    long long t2 = a.y * b.x + b.y * c.x + c.y * a.x;
+    if(t1 - t2 > 0) return 1;
+    else if(t1 - t2 == 0) return 0;
+    else return -1; 
+}
+
+int main(){
+    point p[4];
+    int ans = 0;
+    for(int i = 0; i < 4; i++){
+        cin >> p[i].x >> p[i].y;
+    }
+    if(ccw(p[0], p[1], p[2]) != ccw(p[0], p[1], p[3])){
+        if(ccw(p[2], p[3], p[0]) != ccw(p[2], p[3], p[1])) ans = 1;
+    }
+    cout << ans;
+}
