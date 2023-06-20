fd7f298bfdeabe67011beead13109179f0f38b36
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-06-21 08:13:09 +0900

    [Bronze IV] Title: 치킨 두 마리 (...), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14489. 치킨 두 마리 （．．．）/치킨 두 마리 （．．．）.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if(a + b >= (2 * c)) cout << a + b - (2 * c);
+    else cout << a + b;
+}
