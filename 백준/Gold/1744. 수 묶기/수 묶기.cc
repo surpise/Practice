c9027bf1018ae96bb710979838cc8579873bc163
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-07-04 11:11:10 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int n, x, zero = 0;
+    vector<int> v1, v2;
+    
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> x;
+        if(x > 0) v1.push_back(x);
+        else if(x == 0) zero++;
+        else v2.push_back(x);
+    }
+    sort(v1.begin(), v1.end(), greater<int>());
+    sort(v2.begin(), v2.end());
+    
+    int ans = 0;
+    for(int i = 0; i < v1.size(); i++){
+        if(i == v1.size() - 1) ans += v1[i];
+        else if(v1[i] * v1[i + 1] > v1[i] + v1[i + 1]){
+            ans += v1[i] * v1[i + 1];
+            i++;
+        }
+        else ans += v1[i];
+    }
+    for(int i = 0; i < v2.size(); i ++){
+        if(i == v2.size() - 1) {
+            if(zero > 0) zero--;
+            else ans += v2[i]; 
+        }
+        else if(v2[i] * v2[i + 1] > v2[i] + v2[i + 1]){
+            ans += v2[i] * v2[i + 1];
+            i++;
+        }
+        else ans += v2[i];
+    }
+    cout << ans;
+}
