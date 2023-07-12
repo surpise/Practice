55d4b8a6c817f6a482e98f2a8ab787bca95acc49
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-07-13 08:45:44 +0900

    [Bronze III] Title: 거울, 오! 거울, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4740. 거울， 오！ 거울/거울， 오！ 거울.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    while(1){
+        getline(cin, str);
+        if(str == "***") break;
+        else{
+            for(int i = str.size() - 1; i >= 0; i--){
+                cout << str[i];
+            }
+            cout << '\n';
+        }
+    }
+}
