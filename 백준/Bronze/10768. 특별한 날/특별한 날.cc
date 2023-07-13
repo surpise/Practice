cc1de91c97d2dd238b920dac5f0f1f07b8ceb767
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-07-14 08:40:15 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int m, d;
+    cin >> m >> d;
+    if(m < 2) cout << "Before";
+    else if(m == 2){
+        if(d < 18) cout << "Before";
+        else if(d == 18) cout << "Special";
+        else cout << "After";
+    }
+    else cout << "After";
+}
