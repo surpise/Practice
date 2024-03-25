5d1102e80b8e1893c27bd05ec5cce3a10cde46fb
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-03-25 12:07:00 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 80 ms, Memory: 3588 KB -BaekjoonHub

--- 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <queue>
 
 using namespace std;
 
@@ -14,24 +16,27 @@ void init(){
 }
 
 int solution(){
+  queue<int> q;
+
   int res = 0;
   int l = 0, r = 0;
 
   while(l <= r){
     if(r >= n || l >= n) break;
 
-    if(cnt[arr[r]] < k)
-    {
-      cnt[arr[r]]++;
+    if(cnt[arr[r]] < k){
+          cnt[arr[r]]++;
+      q.push(arr[r]);
       r++;
-    } 
-
+    }
+    
     else{
-      cnt[arr[l]]--;
+      cnt[q.front()]--;
+      q.pop();
       l++;
     }
 
-    res = max(res, r - l);
+    if(q.size() > res) res = q.size();
   }
 
   return res;
