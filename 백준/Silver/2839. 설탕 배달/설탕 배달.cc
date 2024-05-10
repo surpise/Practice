1b17eabc6c44880ba13ad4ab2ef763057948177d
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-05-10 16:38:58 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,49 +1,21 @@
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-    int N, t, ans = 0;
-    cin >> N;
+int main(){
+  int n, ans = 0;
+  cin >> n;
 
-    if(N > 20)
-    {
-        t = (N % 10) + 10;
-        ans += (N - t) / 5;
-        N = t;
+  while(n >= 0){
+    if(n % 5 == 0){
+      ans += n / 5;
+      cout << ans;
+      return 0;
     }
-    
-    if(((N % 19 != 4) && (N % 19 > 2)) || (N % 19 == 0))
-    {
-        ans += (N / 19) * 5;
-        N = N % 19;
-    }
-    if(((N % 15 != 4) && (N % 15 > 2)) || (N % 15 == 0))
-    {
-        ans += (N / 15) * 3;
-        N = N % 15;
-    }
-    if(((N % 12 != 4) && (N % 12 > 2)) || (N % 12 == 0))
-    {
-        ans += (N / 12) * 4;
-        N = N % 12;
-    }
-    if(((N % 8 != 4) && (N % 8 > 2)) || (N % 8 == 0))
-    {
-        ans += (N / 8) * 2;
-        N = N % 8;
-    }
-    if(((N % 5 != 4) && (N % 5 > 2)) || (N % 5 == 0))
-    {
-        ans += (N / 5);
-        N = N % 5;
-    }
-    if(N % 3 == 0)
-    {
-        ans += (N / 3);
-        N = N % 3;
-    }
-    
-    if(N == 0) cout << ans;
-    else cout << "-1";
+
+    n -= 3;
+    ans++;
+  }
+
+  cout << -1;
 }
