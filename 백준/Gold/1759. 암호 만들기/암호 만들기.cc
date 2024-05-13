7939fb253c975ac45b56f0d212b5c30191aaefd3
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2024-05-13 12:18:25 +0900

    [Gold V] Title: 암호 만들기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int L, C;
+char mo[5] = {'a', 'e', 'i', 'o', 'u'};
+char arr[15];
+char res[15];
+bool visited[15];
+
+bool check(){
+  int moCnt = 0, jaCnt = 0;
+  for(int idx = 0; idx < L; idx++){
+    if(find(mo, mo + 5, res[idx]) != mo + 5) moCnt++;
+    else jaCnt++;
+  }
+
+  if(moCnt >= 1 && jaCnt >= 2) return true;
+  else return false;
+}
+
+void search(int depth, int cnt){
+  if(depth >= C){
+    if(cnt == L && check()){
+      for(int idx = 0; idx < L; idx++) cout << res[idx];
+      cout << '\n';
+    }
+    return;
+  }
+
+  res[cnt] = arr[depth];
+  search(depth + 1, cnt + 1);
+  search(depth + 1, cnt);
+}
+
+int main(){
+  cin >> L >> C;
+  for(int idx = 0; idx < C; idx++) cin >> arr[idx];
+
+  sort(arr, arr + C);
+
+  search(0, 0);
+}
